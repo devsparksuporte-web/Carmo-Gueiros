d4ab3dedb08846b4214050f492b46e404295f857
Wesley Silva <wesley@potencial.local>
2026-03-31 10:47:27 -0300

    style: Apply C&G Contabilidade logo and brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: '#eef1fa',
+          100: '#e0e5f6',
+          200: '#c6d1ed',
+          300: '#a1b4e0',
+          400: '#7591cd',
+          500: '#5272ba',
+          600: '#2B4FCB',
+          700: '#2240a6',
+          800: '#1d3587',
+          900: '#192d6e',
+        }
+      }
+    },
   },
   plugins: [],
 };
